fb08a54b77956e05a8fdfce8f328072f38e1276b
marloestacx <marloes.tacx@live.nl>
2022-02-08 17:32:25 +0100

    Comments added

--- visitekaartje/script.js
+++ visitekaartje/script.js
@@ -2,6 +2,7 @@ btn_info = document.getElementById("btn_info");
 front = document.getElementById("front");
 back = document.getElementById("back");
 
+//change class on click
 btn_info.onclick = function () {
   if (!back.classList.contains("hidden")) {
     back.classList.add("hidden");

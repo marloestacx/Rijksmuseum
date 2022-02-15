808402e8d82747cd7b6eb48060f8e91134eed73e
marloestacx <marloes.tacx@live.nl>
2022-02-15 13:35:50 +0100

    Start search function

--- spa/script.js
+++ spa/script.js
@@ -1,8 +1,10 @@
+const data = [];
+
 const display = document.getElementById("items");
 display.textContent = "Laden....";
 
 function getData() {
-  fetch("https://www.rijksmuseum.nl/api/nl/collection?key=c5r1klvi")
+  fetch("https://www.rijksmuseum.nl/api/nl/collection?key=c5r1klvi&ps=100")
     // JSON data
     .then((response) => {
       if (response.status >= 200 && response.status <= 299) {
@@ -12,6 +14,8 @@ function getData() {
     .then((response) => {
       display.textContent = "";
       showData(response.artObjects);
+
+      data.push(response.artObjects);
     })
     .catch(function (err) {
       // Catch error
@@ -23,8 +27,6 @@ getData();
 
 function showData(data) {
   data.forEach((item) => {
-    console.log(item);
-
     newItems = document.createElement("article");
     newItems.innerHTML =
       "<h2>" +
@@ -32,10 +34,18 @@ function showData(data) {
       "</h2><h3>" +
       item.principalOrFirstMaker +
       "</h3><img src=" +
-      item.webImage.url +
+      item.webImage.url.slice(0, -3) +
+      "=s1000" +
       ">";
 
     display.appendChild(newItems);
   });
   // console.log(data);
+}
+
+function search(data) {
+  var input = document.getElementById("myInput");
+  console.log(input.value);
+  console.log(data);
+  data.title.filter(input);
 }

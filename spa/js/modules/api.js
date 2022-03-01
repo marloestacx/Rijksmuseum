2506f7789026c22c66b42c05fc2363cdae424461
marloestacx <marloes.tacx@live.nl>
2022-03-01 17:29:11 +0100

    Search fixed

--- spa/js/modules/api.js
+++ spa/js/modules/api.js
@@ -3,6 +3,8 @@ import { search } from "./search.js";
 const display = document.getElementById("items");
 display.textContent = "Laden....";
 let data = [];
+let globalData;
+export { globalData };
 
 export function getData() {
   fetch("https://www.rijksmuseum.nl/api/nl/collection?key=c5r1klvi&ps=100")
@@ -17,7 +19,8 @@ export function getData() {
       data = response.artObjects;
       console.log(data);
       showData(response.artObjects);
-      search(data);
+      // search(data);
+      globalData = data;
     })
     .catch(function (err) {
       // Catch error
--- spa/js/modules/search.js
+++ spa/js/modules/search.js
@@ -1,14 +1,16 @@
 import { showData } from "./data.js";
+import { globalData } from "./api.js";
 const display = document.getElementById("items");
 
-export function search(data) {
+export function search() {
+  console.log(globalData);
   //Empty results
   display.innerHTML = "";
 
   var input = document.getElementById("myInput");
 
   //search on input
-  var search = data.filter(function (d) {
+  var search = globalData.filter(function (d) {
     return (
       d.title.toLowerCase().includes(input.value.toLowerCase()) ||
       d.principalOrFirstMaker.toLowerCase().includes(input.value.toLowerCase())

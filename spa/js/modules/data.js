cf1a72aa3607e475987707030827300030d41cae
marloestacx <marloes.tacx@live.nl>
2022-03-08 13:40:14 +0100

    innerHTML changed to insertAdjacentHTML

--- spa/js/modules/data.js
+++ spa/js/modules/data.js
@@ -2,16 +2,16 @@ export function showData(data) {
   const display = document.getElementById("items");
 
   data.forEach((item) => {
-    let newItems = document.createElement("article");
-    newItems.innerHTML =
-      "<h2>" +
-      item.title +
-      "</h2><h3>" +
-      item.principalOrFirstMaker +
-      "</h3><img src=" +
-      item.webImage.url.slice(0, -3) +
-      "=s1000" +
-      ">";
-    display.appendChild(newItems);
+    display.insertAdjacentHTML(
+      "beforeend",
+      "<article><h2>" +
+        item.title +
+        "</h2><h3>" +
+        item.principalOrFirstMaker +
+        "</h3><img src=" +
+        item.webImage.url.slice(0, -3) +
+        "=s1000" +
+        "></article"
+    );
   });
 }

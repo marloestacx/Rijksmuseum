fb38561f91e186029853ce587a6a20cdb6c4b066
marloestacx <marloes.tacx@live.nl>
2022-03-09 19:43:36 +0100

    loading url changed

--- spa/js/modules/states.js
+++ spa/js/modules/states.js
@@ -1,7 +1,7 @@
 const display = document.getElementById("items");
 
 export function loading() {
-  display.innerHTML = "<img src='../images/loading.gif'/>";
+  display.innerHTML = "<img src='./images/loading.gif'/>";
 }
 
 export function errorSearch() {
